634d6fb2b333fd8621156607b1dfb225cb30d149
Cristian Mircea Messel <mess110@gmail.com>
2014-09-21 23:00:38 +0300

    release script

---
+++ release.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'json'
+
+bower = JSON.parse(File.read("bower.json"))
+version = bower['version']
+
+`coffee --output build/ -b -c src/`
+`git add build/`
+`git commit -m "release version #{version}"`
+`git tag #{version}`
+`git push --tags`
+`git push origin master`
