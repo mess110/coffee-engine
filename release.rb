de2a21e45a833c798d2313ca61a9fbd8349cd9f7
Cristian Mircea Messel <mess110@gmail.com>
2014-09-21 23:23:44 +0300

    fix release script

--- release.rb
+++ release.rb
@@ -7,6 +7,7 @@ bower = JSON.parse(File.read("bower.json"))
 version = bower['version']
 
 `coffee --output build/ -b -c src/`
+`git add bower.json`
 `git add build/`
 `git commit -m "release version #{version}"`
 `git tag #{version}`
